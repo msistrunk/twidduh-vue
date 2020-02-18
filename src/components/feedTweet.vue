88d3e5d75d364ca1d373158659ca7097201efae1
Mitchell Sistrunk <sistrunkmitchell@gmail.com>
2020-02-17 22:42:00 -0600

    Add tweet component for feed

---
+++ src/components/feedTweet.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="tweet">
+    <div class="profilePicture">
+      <p><font-awesome-icon :icon="['fas', 'user-circle']" /></p>
+    </div>
+    <div class="tweetText">
+      <p class="username">
+        @username
+      </p>
+      <p class="tweetBody">
+        {{ tweet.text }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faUserCircle)
+
+export default {
+  name: 'feedTweet',
+  data: function () {
+    return {
+    }
+  },
+  props: {
+    tweet: Object,
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .tweet {
+    display: grid;
+    outline-style: solid;
+    grid-template-columns: 50px auto;
+  }
+  .tweetText {
+    text-align: left;
+  }
+</style>
